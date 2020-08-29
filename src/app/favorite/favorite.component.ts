10165deee998de9e308d3e9712e1d06158d23a91
Dijo Davis <dijodevpd@gmail.com>
2020-08-29 17:10:20 +0530

    Component Input Props

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  private isClicked: Boolean = false;
+  @Input('isFavorite') private isClicked: Boolean = false;
 
   constructor() { }
 
